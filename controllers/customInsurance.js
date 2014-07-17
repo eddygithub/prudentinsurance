223a4c031ba6693dda82ecaf3f82b7b4d6ed9e99
Eddy Zhu <eddygithub@gmail.com>
2014-07-16 22:40:49 -0400

    add angular ui-template

--- controllers/customInsurance.js
+++ controllers/customInsurance.js
@@ -4,30 +4,44 @@
  use to handle customer
 */
 
-		var prudentInsuranceApp = angular.module('prudentInsurance', ['ngRoute']);
+		var prudentInsuranceApp = angular.module('prudentInsurance', ['ui.router']);
 
-		prudentInsuranceApp.config(function ($routeProvider){
-			$routeProvider
-			.when('/home',
+		prudentInsuranceApp.config(function ($stateProvider, $urlRouterProvider){
+
+			$urlRouterProvider.otherwise("/home");
+
+			$stateProvider
+			.state('home',
 				{
+					url: '/home',
 					controller: 'MainController',
 					templateUrl: 'partials/home.html'
 				})
-			.when('/about', 
+			.state('about', 
 				{	
+					url: '/about',
 					controller: 'MainController',
 					templateUrl: 'partials/about.html'
 				})
-			.when('/contact', 
-				{	
+			.state('contact', 
+				{
+					url: '/contact',	
 					controller: 'MainController',
 					templateUrl: 'partials/contact.html'
 				})
-			.otherwise({ redirectTo: '/home'});
 		});
 
 
 		prudentInsuranceApp.controller('MainController', function ($scope){
+			$scope.open = function() {
+			  $scope.showModal = true;
+			};
 
-			
+			$scope.ok = function() {
+			  $scope.showModal = false;
+			};
+
+			$scope.cancel = function() {
+			  $scope.showModal = false;
+			};
 		});
